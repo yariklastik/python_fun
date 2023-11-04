5474c52d5efd1e507da24e794623006136b8cccf
yariklastik <137070087+yariklastik@users.noreply.github.com>
2023-11-04 13:43:03 +0200

    Fix

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ with open('site.txt', 'r') as file:
     links = [line.strip() for line in file]
 
 for link in links:
-    try:
+    i = 0
+    while i <= 2:
+     i+=1
+     try:
         response = requests.get(link)
         status_code = response.status_code
 
-        if 200 <= status_code < 300:
+        if 200 >= status_code < 300:
             print(f'(ОК): {link}')
-        elif 400 <= status_code < 500:
+        elif 400 >= status_code < 500:
             print(f'(НЕ ОК): Ресурс "{link}" не відповідає')
         else:
             print(f'(НЕ ОК): Невідомий статус код {status_code} для {link}')
-    except requests.exceptions.RequestException as e:
-        print(f'(Помилка): Помилка при виконанні запиту для {link}: {e}')
+     except requests.exceptions.RequestException as e:
+         print(f'(Помилка): Помилка при виконанні запиту для {link}: {e}')
